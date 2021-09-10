79f19fc0e59f30b44c8b88bf2a1b4fa56816428b
Kamil Kmetyk <kamil.kmetyk@darkfox.pl>
2021-09-11 00:39:41 +0200

    Map configuration class simplification.

--- src/main/scala/core/configuration/Map.scala
+++ src/main/scala/core/configuration/Map.scala
@@ -7,13 +7,10 @@ class Map:
   val height: Array[Int] = Array( 128, 256 )
 
   def generateRoomsAmount: Int =
-    val random = scala.util.Random
-    return random.between(this.rooms.apply(0), this.rooms.apply(1))
+    return scala.util.Random.between(this.rooms.apply(0), this.rooms.apply(1))
 
   def generateMapWidth: Int =
-    val random = scala.util.Random
-    return random.between(this.width.apply(0), this.width.apply(1))
-  
+    return scala.util.Random.between(this.width.apply(0), this.width.apply(1))
+
   def generateMapHeight: Int =
-    val random = scala.util.Random
-    return random.between(this.height.apply(0), this.height.apply(1))
+    return scala.util.Random.between(this.height.apply(0), this.height.apply(1))
